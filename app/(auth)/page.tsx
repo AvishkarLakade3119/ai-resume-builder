36e847914e788ee6e48baf9d23591da57d34de44
AvishkarLakade3119 <90135361+AvishkarLakade3119@users.noreply.github.com>
2025-07-16 15:02:01 +0530

    Update page.tsx

--- app/(auth)/page.tsx
+++ app/(auth)/page.tsx
@@ -115,7 +115,9 @@ const page = () => {
             </span>
             . All Rights Reserved.
           </span>
-          <Link href="https://github.com/exclusiveabhi" className="me-4 md:me-6">
+          <Link href="www.linkedin.com/in/avishkar-lakade-562a29203
+
+" className="me-4 md:me-6">
             <span className="hover:text-primary-500 mt-3 text-sm font-medium text-gray-500 sm:mt-0">
               Made by Avishkar Lakade!
             </span>
